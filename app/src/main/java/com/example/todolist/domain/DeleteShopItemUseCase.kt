ac14218ee1ba33a53f419e23b219feff991f269d
HV220 <diagenin@gmail.com>
2024-03-03 20:57:32 +0300

    added useCase

---
+++ app/src/main/java/com/example/todolist/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.todolist.domain
+
+class DeleteShopItemUseCase {
+    fun deleteShopItem(shopItem: ShopItem): Boolean {
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/todolist/domain/GetShopListUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.todolist.domain
+
+class GetShopListUseCase {
+    fun getShopList(): List<ShopItem> {
+        TODO()
+    }
+}
