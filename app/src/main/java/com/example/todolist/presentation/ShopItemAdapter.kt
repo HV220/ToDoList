8e6877a42c11f51f6c634a41310b3cb4b20ccfc4
HV220 <diagenin@gmail.com>
2024-03-06 00:40:48 +0300

    added ShopListDiffCallback.kt redacted ShopListRepositoryImpl.kt

--- app/src/main/java/com/example/todolist/presentation/ShopItemAdapter.kt
+++ app/src/main/java/com/example/todolist/presentation/ShopItemAdapter.kt
@@ -1,31 +1,38 @@
 package com.example.todolist.presentation
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.todolist.R
 import com.example.todolist.domain.ShopItem
 
-class ShopItemAdapter() :
+class ShopItemAdapter :
     RecyclerView.Adapter<ShopItemAdapter.ShopItemViewHolder>() {
     companion object {
         const val ENABLED_ITEM = 1
         const val DISABLED_ITEM = 0
         const val POOL_VIEW_HOLDER = 15
+        const val TAG = "ShopItemAdapter"
     }
-var count = 0
+
     var shopItems: List<ShopItem> = arrayListOf()
         set(value) {
+            val shopListDiff = ShopListDiffCallback(shopItems, value)
+            val resultDiff = DiffUtil.calculateDiff(shopListDiff)
+
+            resultDiff.dispatchUpdatesTo(this@ShopItemAdapter)
+
             field = value
-            notifyDataSetChanged()
         }
 
+    var onLongClickListenerShopItem: ((ShopItem) -> Unit)? = null
+    var onClickListenerShopItem: ((ShopItem) -> Unit)? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        Log.d("ShopItemAdapter", "${++count}")
         val binding = when (viewType) {
             ENABLED_ITEM -> R.layout.item_shop_enabled
             DISABLED_ITEM -> R.layout.item_shop_disabled
@@ -38,10 +45,22 @@ var count = 0
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+
         val shopItem = shopItems[position]
-        holder.textDescriptionDo.text = shopItem.name
-        holder.textCountDo.text = shopItem.count.toString()
+
+        with(holder) {
+            textDescriptionDo.text = shopItem.name
+            textCountDo.text = shopItem.count.toString()
+
+            itemView.setOnLongClickListener {
+                onLongClickListenerShopItem?.invoke(shopItem)
+                true
+            }
+
+            itemView.setOnClickListener { onClickListenerShopItem?.invoke(shopItem) }
+        }
     }
+
 
     override fun getItemViewType(position: Int): Int {
         val item = shopItems[position]
@@ -52,7 +71,7 @@ var count = 0
 
     class ShopItemViewHolder(view: View) :
         ViewHolder(view) {
-        var textDescriptionDo = view.findViewById<TextView>(R.id.text_description_do)
-        var textCountDo = view.findViewById<TextView>(R.id.text_count_do)
+        var textDescriptionDo: TextView = view.findViewById(R.id.text_description_do)
+        var textCountDo: TextView = view.findViewById(R.id.text_count_do)
     }
 }
