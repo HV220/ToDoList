1e52e827104118dd028e435502b029da3e41cd18
HV220 <diagenin@gmail.com>
2024-03-06 23:10:20 +0300

    redacted shop_item functions added LiveData

--- app/src/main/java/com/example/todolist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/todolist/presentation/ShopItemActivity.kt
@@ -4,17 +4,21 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.todolist.R
 
 class ShopItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+
+        var model = ViewModelProvider(this@ShopItemActivity)[ShopItemViewModel::class.java]
+
     }
 
     companion object {
 
-        fun getIntent(context: Context) : Intent {
+        fun getIntent(context: Context): Intent {
             return Intent(context, ShopItemActivity::class.java)
         }
     }
